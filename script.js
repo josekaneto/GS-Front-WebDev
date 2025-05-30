deeb2a529e40a1f44ec9ac90c02f8c50e08943ce
José Otávio Kaneto <josekaneto0828@gmail.com>
2025-05-29 22:43:12 -0300

    Mudanças nas imagens do slideshow e seção 4 - Público-alvo fin alizada

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let i = 0
 let image = [
     'images/casas-inundadas.jpg', // Antes
-    'images/inundacao-carro.jpg', // Depois
-    'images/placa-inundada.jpg', // Antes
-    'images/placa-inundada.jpg' // Depois
+    'images/cidade-sem-agua.png', // Depois
+    'images/inundacao-carro.jpg', // Antes
+    'images/carro-sem-agua.png' // Depois
 ]
 let labels = [
     'Antes',
@@ -11,11 +11,19 @@ let labels = [
     'Antes',
     'Depois'
 ]
+
+let alts = [
+    'Imagem de casas inundadas',
+    'Imagem de um carro inundado',
+    'Imagem de uma placa inundada',
+    'Imagem de uma placa inundada'
+]
 let time = 3000
 
 function changeImg() {
     document.getElementById("slide").src = image[i]
     document.getElementById("antes-depois-subtitulo").textContent = labels[i]
+    document.getElementById("slide").alt = labels[i]
 
     if (i < image.length - 1) {
         i++
