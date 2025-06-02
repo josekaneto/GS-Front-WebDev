d0bd86f5c6376098152c84ff73ab804bd12e50c1
José Otávio Kaneto <josekaneto0828@gmail.com>
2025-06-02 00:40:24 -0300

    Tela de quiz finalizada

---
+++ quiz.js
@@ -0,0 +1,163 @@
+let questoes = [{
+        question: "Qual das alternativas é uma das principais causas das enchentes nas cidades?",
+        answers: [{
+                option: "Impermeabilização do solo (asfalto, concreto)",
+                correct: true
+            },
+            {
+                option: "Ventos fortes",
+                correct: false
+            },
+            {
+                option: "Plantio de árvores em excesso",
+                correct: false
+            },
+        ],
+    },
+    {
+        question: "Qual medida ajuda a prevenir enchentes nas áreas urbanas?",
+        answers: [{
+                option: "Jogar lixo nos bueiros",
+                correct: false
+            },
+            {
+                option: "Preservar áreas verdes e construir sistemas de drenagem",
+                correct: true
+            },
+            {
+                option: "Construir mais prédios",
+                correct: false
+            },
+        ],
+    },
+    {
+        question: "O lixo jogado nas ruas contribui para as enchentes porque:",
+        answers: [{
+                option: "Deixa a cidade mais feia",
+                correct: false
+            },
+            {
+                option: "Deixa o solo mais fértil",
+                correct: false
+            },
+            {
+                option: "Obstrui bueiros e impede o escoamento da água",
+                correct: true
+            },
+        ],
+    },
+    {
+        question: "Quando ocorre uma enchente, é recomendado:",
+        answers: [{
+                option: "Atravessar a pé ou de carro áreas alagadas",
+                correct: false
+            },
+            {
+                option: "Ficar próximo de postes e fios elétricos",
+                correct: false
+            },
+            {
+                option: "Procurar abrigo seguro, longe da água",
+                correct: true
+            },
+        ],
+    },
+    {
+        question: "Qual destes fenômenos não está diretamente relacionado às enchentes?",
+        answers: [{
+                option: "Poluição do ar",
+                correct: true
+            },
+            {
+                option: "Chuvas fortes",
+                correct: false
+            },
+            {
+                option: "Falta de drenagem urbana",
+                correct: false
+            },
+        ],
+    },
+];
+
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+const btnVoltar = document.querySelectorAll(".finish button")[1];
+
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+btnRestart.onclick = () => {
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
+
+    currentIndex = 0;
+    questionsCorrect = 0;
+    loadQuestion();
+};
+
+function nextQuestion(e) {
+    if (e.target.getAttribute("data-correct") === "true") {
+        questionsCorrect++;
+    }
+
+    if (currentIndex < questoes.length - 1) {
+        currentIndex++;
+        loadQuestion();
+    } else {
+        finish();
+    }
+}
+
+function finish() {
+    let motivacional = "";
+    if (questionsCorrect <= 2) {
+        motivacional = `<span>
+            Você pode melhorar! Pratique mais bons hábitos para prevenir enchentes e proteger nossa comunidade!
+        </span>`;
+    } else {
+        motivacional = `<span>
+            Parabéns! Continue praticando bons hábitos para prevenir enchentes e proteger nossa comunidade!
+        </span>`;
+    }
+
+    textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questoes.length}<br><br>${motivacional}`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+    spnQtd.innerHTML = `${currentIndex + 1}/${questoes.length}`;
+    const item = questoes[currentIndex];
+    answers.innerHTML = "";
+    question.innerHTML = item.question;
+
+    item.answers.forEach((answer) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+<button class="answer" data-correct="${answer.correct}">
+    ${answer.option}
+</button>
+`;
+
+        answers.appendChild(div);
+    });
+
+    document.querySelectorAll(".answer").forEach((item) => {
+        item.addEventListener("click", nextQuestion);
+    });
+
+    btnVoltar.onclick = () => {
+        window.close();
+    };
+
+}
+
+loadQuestion();
---
+++ script.js
@@ -0,0 +1,44 @@
+let i = 0
+let image = [
+    'images/casas-inundadas.jpg', // Antes
+    'images/cidade-sem-agua.png', // Depois
+    'images/inundacao-carro.jpg', // Antes
+    'images/carro-sem-agua.png' // Depois
+]
+let labels = [
+    'Antes',
+    'Depois',
+    'Antes',
+    'Depois'
+]
+
+let alts = [
+    'Imagem de casas inundadas',
+    'Imagem da casas sem água',
+    'Imagem de um carro inundado',
+    'Imagem do carro sem água'
+]
+let time = 3000
+
+function changeImg() {
+    document.getElementById("slide").src = image[i]
+    document.getElementById("antes-depois-subtitulo").textContent = labels[i]
+    document.getElementById("slide").alt = alts[i]
+
+    if (i < image.length - 1) {
+        i++
+    } else {
+        i = 0
+    }
+
+    setTimeout(changeImg, time)
+}
+window.onload = changeImg;
+
+
+
+document.querySelector('.menu-hamburguer').addEventListener('click', function () {
+    document.querySelector('.lista-menu').classList.toggle('ativo');
+});
+
+
